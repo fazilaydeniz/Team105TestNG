ef7ec0ef7b732e78905ca0074e069abc48edff7d
fazilaydeniz <fazilaydeniz@hotmail.com>
2023-01-10 14:09:43 +0300

    10.01.2023

---
+++ src/test/java/tests/day18htmlReports/C01_HTML_Reports.java
@@ -0,0 +1,37 @@
+package tests.day18htmlReports;
+
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.TestBaseRapor;
+
+public class C01_HTML_Reports extends TestBaseRapor {
+    //TestNGden direkt rapor alamıyoruz. İlla almak istiyorsak extentreport kütüphanesini dosyamıza ekledik.
+    // bu şekilde onu kullanarak rapor oluşturabiliyoruz. Rapor oluşturabilmek için de önce html raporu  kendi ellerimizle
+    //oluşturmamız gerekiyor.(Aşağıda infolarla oluşturduğumuz durumlar)
+
+    @Test
+    public void test01(){
+
+        extentTest= extentReports.createTest("Nutella testi","Kullanici amazonda Nutella arayabilmeli");
+
+        // amazona gidelim
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        extentTest.info("Amazon anasayfaya gidildi");
+        // Nutella aratalim
+        AmazonPage amazonPage= new AmazonPage();
+        amazonPage.amazonAramaKutusu.sendKeys("Nutella" + Keys.ENTER);
+        extentTest.info("Arama kutusuna Nutella yazip, arama yapildi");
+        // sonuclarin nutella icersigini test edelim
+
+        String actualSonucYazisi= amazonPage.aramasonucuElementi.getText();
+        String expectedKelime= "Nutella";
+        extentTest.info("Arama sonuclari kaydedildi");
+
+        Assert.assertTrue(actualSonucYazisi.contains(expectedKelime));
+        extentTest.pass("Arama sonuclarinin Nutella icerdigi test edildi");
+    }
+}
